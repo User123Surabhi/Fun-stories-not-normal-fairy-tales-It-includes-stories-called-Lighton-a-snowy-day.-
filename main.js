a56731d4e4e20ad34910b64a9c5782868fe79e56
Sweet_Spy_Sandy <88492457+User123Surabhi@users.noreply.github.com>
2022-01-18 18:32:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+function KaylaNext()
+{
+   window.location = "story2.html";
+}
+function stories()
+{
+   window.location = "story1.html";
+}
+function FraniNext()
+{
+   window.location = "story3.html";
+}
+function LazyNext()
+{
+    window.location = "story4.html";
+}
+function RoosterNext()
+{
+    window.location = "story5.html"
+}
+function CrocodileNext()
+{
+    window.location = "story6.html"
+}
+function BluesNext()
+{
+    window.location = "story7.html"
+}
+function SkunkNext()
+{
+    window.location = "story8.html"
+}
+function shellNext()
+{
+    window.location = "story9.html"
+}
+function turkeyNext()
+{
+    window.location = "story10.html"
+}
+function winstonNext()
+{
+    window.location = "story11.html"
+}
+function alligatorNext()
+{
+    window.location = "story12.html"
+}
+function spencerNext()
+{
+    window.location = "story13.html"
+}
+function bipperNext()
+{
+    window.location = "story14.html"
+}
+function snowyNext()
+{
+    window.location = "story15.html"
+}
+function larkinNext()
+{
+    window.location = "story16.html"
+}
+function waspNext()
+{
+    window.location = "end.html"
+}
+function restart()
+{
+    window.location = "Home_page.html"
+}
